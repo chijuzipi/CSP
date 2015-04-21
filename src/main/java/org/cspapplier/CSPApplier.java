a1f180dc1d623e29a17180c97fd6b91b113cab1f
archlyx <archlyx@gmail.com>
2015-04-21 17:12:45 -0500

    Refactor the structure of CSPapplier

--- src/main/java/org/cspapplier/CSPApplier.java
+++ src/main/java/org/cspapplier/CSPApplier.java
@@ -4,34 +4,47 @@ import org.cspapplier.json.HashMapInJson;
 import org.cspapplier.json.JsonAnalyzer;
 import org.cspapplier.json.JsonWriter;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 /**
  *  CSPApplier.java
  *
  *  The main class for processing the HTML/JS/CSS
  */
 
-public class CSPApplier {
-    URLContentAnalyzer getURL;
-
-    public CSPApplier(String inputHTML, String url, String outputPath, String httpPath) throws Exception {
-        // Generate elements for external / block / inline JS
-        getURL = new URLContentAnalyzer(inputHTML, url, outputPath);
-        getURL.generateJSElements();
-        getURL.generateCSSElements();
-
 //    	if (args.length != 5) {
 //    		System.out.println("Usage: Java CSPApplier [Input file] [Output file path] " +
 //                               "[HTTP Path] [URL] [isSampleMode = 0 | 1]");
 //    		return;
 //    	}
-        boolean isSample =  true;
 
+public class CSPApplier {
+    URLContentAnalyzer getURL;
+    URLContentGenerator htmlGen;
+    CSPGenerator cspGen;
+
+    HashMapGenerator elementHashMap;
+    HashMapInJson jsonFromRequest;
+
+    public CSPApplier(String inputHTML, String url, String outputPath, String httpPath) throws Exception {
+        /*
+         *  Query the elements with external / block / inline JS
+         */
+        getURL = new URLContentAnalyzer(inputHTML, url, outputPath);
+        getURL.generateJSElements();
+        getURL.generateCSSElements();
+
+        elementHashMap = new HashMapGenerator();
+        jsonFromRequest = new HashMapInJson();
+    }
+
+    public void analyzeJson() throws Exception {
         /*
          * Generate HashMaps for external / block / inline JS
          * - Key: the SHA1 hash of the JS content
          * - Value: the Elements or Array<ElementEventBinder> object
          */
-        HashMapGenerator elementHashMap = new HashMapGenerator();
         elementHashMap.generateJSElementHashMap(getURL);
         elementHashMap.generateCSSElementHashMap(getURL);
 
@@ -40,7 +53,6 @@ public class CSPApplier {
          * - Key: the SHA1 hash of the JS content
          * - Value: the Array<ElementInJson> object which is Json compatible
          */
-        HashMapInJson jsonFromRequest = new HashMapInJson();
         jsonFromRequest.convertJS(elementHashMap);
         jsonFromRequest.convertCSS(elementHashMap);
 
@@ -67,14 +79,33 @@ public class CSPApplier {
             JsonWriter jsonWriter = new JsonWriter(jsonFromRequest, getURL.getHashURL(), getURL.getOutputPath());
             jsonWriter.write();
         }
-        
-        URLContentGenerator htmlGen = new URLContentGenerator(getURL, elementHashMap, httpPath);
-        htmlGen.generateJS();
-        htmlGen.generateCSS();
+
+        /*
+         * Write new HTML/JS/CSS files to the files
+         */
+        htmlGen = new URLContentGenerator(getURL, elementHashMap, httpPath);
+    }
+
+    public String generateHTML() throws Exception {
         htmlGen.generateHTML();
-        
-        CSPGenerator cspGen = new CSPGenerator(getURL);
+        return newHTML;
+    }
+
+    public void generateJS() throws Exception {
+        htmlGen.generateJS();
+    }
+
+    public void generateCSS() throws Exception {
+        htmlGen.generateCSS();
+    }
+
+    public String generateCSP() {
+        /*
+         * Generate CSP header
+         */
+        cspGen = new CSPGenerator(getURL);
         cspGen.generateCSPHeader();
         cspGen.write();
+        return cspHeader;
     }
 }
