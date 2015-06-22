5fbcd67004b02fb42dde9afc3ee040848d5368bc
archlyx <archlyx@gmail.com>
2015-06-22 17:38:17 -0500

    Multi-string comparison works.

--- src/main/java/org/cspapplier/util/ScriptDiff.java
+++ src/main/java/org/cspapplier/util/ScriptDiff.java
@@ -19,18 +19,33 @@ public class ScriptDiff {
         this.stringArray = stringArray;
     }
 
-    public String TwoStringDiff(String s1, String s2) {
+    public String TwoStringIntersect(String s1, String s2) {
         DiffMatchPatch dmp = new DiffMatchPatch();
         dmp.Diff_Timeout = 16;
         LinkedList<Diff> diffs = dmp.diff_main(s1, s2);
+        dmp.diff_cleanupSemantic(diffs);
 
         String overlap = "";
-
         for (Diff diff : diffs) {
             if (diff.operation.equals(DiffMatchPatch.Operation.EQUAL))
                 overlap += diff.text;
         }
 
         return overlap;
+    }
+
+    public String MultiStringIntersect() {
+        if (stringArray.size() < 2)
+            return stringArray.get(0);
+        else {
+            String s1 = stringArray.get(0);
+            String s2 = stringArray.get(1);
+            String overlap = TwoStringIntersect(s1, s2);
+
+            for (int i = 2; i < stringArray.size(); i++)
+                overlap = TwoStringIntersect(overlap, stringArray.get(i));
+
+            return overlap;
+        }
     }
 }
--- src/test/java/org/cspapplier/util/ScriptDiffTest.java
+++ src/test/java/org/cspapplier/util/ScriptDiffTest.java
@@ -14,20 +14,60 @@ import static org.junit.Assert.*;
  */
 public class ScriptDiffTest {
 
-    private ArrayList<String> simpleCases;
-    private ScriptDiff scriptDiff;
+    private ArrayList<String> singleCase;
+    private ArrayList<String> simpleCase;
+    private ArrayList<String> multiCase1;
+    private ArrayList<String> multiCase2;
 
     @Before
     public void setUp() {
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("aaabbccc");
-        list.add("aaadfssfdsdccc");
-        scriptDiff = new ScriptDiff(list);
+
+        singleCase = new ArrayList<String>();
+        singleCase.add("abcd");
+
+        simpleCase = new ArrayList<String>();
+        simpleCase.add("aaabbccc");
+        simpleCase.add("aaadfssfdsdccc");
+
+        multiCase1 = new ArrayList<String>();
+        multiCase1.add("aaabbccc");
+        multiCase1.add("aaadfsfdccc");
+        multiCase1.add("aacbbcc");
+
+        multiCase2 = new ArrayList<String>();
+        multiCase2.add("{\"attr\": 3, \"token\": 829432, \"test\": \"no\" }");
+        multiCase2.add("{\"attr\": 2, \"token\": 422522g, \"test\": \"yes\" }");
+        multiCase2.add("{\"attr\": 3, \"token\": g234243, \"test\": \"no\" }");
+        multiCase2.add("{\"attr\": 2, \"token\": 43hg334, \"test\": \"no\" }");
+        multiCase2.add("{\"attr\": 1, \"token\": gwr32234, \"test\": \"yes\" }");
+
     }
 
     @Test
-    public void testTwoStringDiff() throws Exception {
-        String overlap = scriptDiff.TwoStringDiff("aaabbccc", "aaadfdsfsddccc");
+    public void testTwoStringIntersect() throws Exception {
+        ScriptDiff scriptDiff = new ScriptDiff(simpleCase);
+        String overlap = scriptDiff.TwoStringIntersect(simpleCase.get(0), simpleCase.get(1));
         assertEquals("aaaccc", overlap);
     }
+
+    @Test
+    public void testMultiStringIntersect() throws Exception {
+
+        ScriptDiff scriptDiff_edge = new ScriptDiff(singleCase);
+        String overlap_edge = scriptDiff_edge.MultiStringIntersect();
+        assertEquals("abcd", overlap_edge);
+
+        ScriptDiff scriptDiff1 = new ScriptDiff(multiCase1);
+        String overlap1 = scriptDiff1.MultiStringIntersect();
+        assertEquals("aaccc", overlap1);
+
+        ScriptDiff scriptDiff2 = new ScriptDiff(multiCase2);
+        String overlap2 = scriptDiff2.MultiStringIntersect();
+        assertEquals("{\"attr\": , \"token\": , \"test\": \"\" }", overlap2);
+
+        ScriptDiff scriptDiff3 = new ScriptDiff(simpleCase);
+        String overlap3 = scriptDiff3.MultiStringIntersect();
+        assertEquals("aaaccc", overlap3);
+    }
+
 }
